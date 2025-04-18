c788c0ab5f3d7c49210e8762c15cf5a9cbd01b32
Vartika Singh <svartika841@gmail.com>
2025-04-18 19:01:37 -0400

    Time: 9 ms (89.57%), Space: 60.4 MB (12.39%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -2,6 +2,7 @@ import java.util.HashSet;
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        int capacity = (int)(nums.length / 0.75f) + 1;
         HashSet<Integer> seen = new HashSet<>();
 
    for (int num : nums) {
