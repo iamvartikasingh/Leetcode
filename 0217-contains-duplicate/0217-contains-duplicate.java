abab0c5a6ed3a85e35f2906f2b6e946ab4c29086
Vartika Singh <svartika841@gmail.com>
2025-04-18 19:01:14 -0400

    Time: 9 ms (89.57%), Space: 60.9 MB (7.08%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -4,12 +4,12 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> seen = new HashSet<>();
 
-for(int num:nums){
-    if(seen.contains(num)){
-       return true;
-    }
-  seen.add(num);
-}
+   for (int num : nums) {
+            if (!seen.add(num)) {
+                return true; // duplicate found
+            }
+        }
+
 return false;
         }
     
