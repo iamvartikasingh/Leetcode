466e2dfe9ed164b2d6a48dc257bfae834bde6d33
Vartika Singh <svartika841@gmail.com>
2025-11-11 00:27:35 -0500

    Time: 7 ms (82.46%), Space: 18.2 MB (45.26%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,23 +1,27 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-
-        x=0
-        y=len(s)-1
-        while x<y:
-            while x<y and not self.alphaNum(s[x]):
-                x+=1
-            while y>x and not self.alphaNum(s[y]):
-                y-=1
-            if s[x].lower() !=s[y].lower():
-                return False
+        newStr = ""
+        for c in s:
+            if c.isalnum():
+                newStr+=c.lower()
+        return newStr==newStr[::-1]
+    #     x=0
+    #     y=len(s)-1
+    #     while x<y:
+    #         while x<y and not self.alphaNum(s[x]):
+    #             x+=1
+    #         while y>x and not self.alphaNum(s[y]):
+    #             y-=1
+    #         if s[x].lower() !=s[y].lower():
+    #             return False
            
-            x+=1
-            y-=1
-        return True
-    def alphaNum(self, c):
-        return(ord('A') <= ord(c)<= ord('Z')
-               or ord('a') <= ord(c)<= ord('z') or
-               ord('0') <= ord(c)<= ord('9'))
+    #         x+=1
+    #         y-=1
+    #     return True
+    # def alphaNum(self, c):
+    #     return(ord('A') <= ord(c)<= ord('Z')
+    #            or ord('a') <= ord(c)<= ord('z') or
+    #            ord('0') <= ord(c)<= ord('9'))
 
 
         
